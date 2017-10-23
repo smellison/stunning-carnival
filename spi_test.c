f383516293fe03318e7f6a15ee8d5fe9bcbc7deb
ellison <gentlemenbandits@gmail.com>
2017-10-23 05:54:19 +0000

    Added some start up stuff to the spi_test.c the test mode at least seems to work correctly when I drop the scan limit to two, but I can't seem to get an isolated digit write to work without seeming buggy

--- spi_test.c
+++ spi_test.c
@@ -10,14 +10,35 @@
 #define CHANNEL 0
 
 void main(int argc, char** argv) {
-	uint8_t on[] = { 0xFF, 0xFF };
+
+        /*
+        Digit 0 is selected via 0xX1
+        Digit 1 is selected via 0xX2 and so on
+        the number 3 is written as 01111001 or 79
+        */
+
+	/*uint8_t on[] = { 0x01, 0x79 };*/
+        uint8_t on[] = { 0xFF, 0xFF };
 	uint8_t off[] = { 0xFF, 0x00 };
 	uint8_t buf[2];
 
-	if (wiringPiSPISetup(CHANNEL, 1000000) < 0) {
+	if (wiringPiSPISetup(CHANNEL, 8000000) < 0) {
 		fprintf (stderr, "SPI Setup failed: %s\n", strerror(errno));
 		exit(errno);
 	}
+        
+        uint8_t intensity = { 0x0A, 0x05 }
+        uint8_t scan_limit = { 0x0B, 0x01 } 
+        uint8_t shutdown = { 0x0C, 0x01 }
+
+        memcpy(buf, scan_limit, 2);
+        wiringPiSPIDataRW(CHANNEL, buf, 2);
+
+        memcpy(buf, intensity, 2);
+        wiringPiSPIDataRW(CHANNEL, buf, 2);
+
+        memcpy(buf, shutdown, 2);
+        wiringPiSPIDataRW(CHANNEL, buf, 2);    
 
 	for (;;) {
 		memcpy(buf, on, 2);
