5062b9d8385ec9edb86c9dbc5b6c2b4c4bfa6d65
ellison <gentlemenbandits@gmail.com>
2017-10-26 05:42:33 +0000

    Common anode solution found for at least one chip driving, have to cumulatively bitwise and the data to be set or unset (GOD DAMN) for each "digit" aka segment to be turned on or off Its a huge pain in the ass oh my god what is wrong with me

--- spi_test.c
+++ spi_test.c
@@ -18,7 +18,7 @@ void spi(uint8_t reg, uint8_t val) {
     wiringPiSPIDataRW(CHANNEL, buf, 2);
     /*printf("buf zero %x\n", buf[0]);*/
     /*printf("buf one %x\n", buf[1]);*/
-    usleep(50);
+    /*usleep(50);*/
 }
 
 void shutdown() {
@@ -29,13 +29,7 @@ void main(int argc, char** argv) {
 
         /*
         Digit 0 is selected via 0xX1
-        Digit 1 is selected via 0xX2 and so on
-        the number 3 is written as 01111001 or 79
 
-        H is 37
-        I is 30
-
-        TODO solve random failure issue. Caps? resistors? sleeps?
         */
 
 	if (wiringPiSPISetup(CHANNEL, 1000000) < 0) {
@@ -46,8 +40,8 @@ void main(int argc, char** argv) {
         /* set bcd OFF */
         spi(0x09, 0x00);
 
-        /* set scan limit */
-        spi(0x0B, 0x01);
+        /* set scan limit to one digit for now */
+        spi(0x0B, 0x07);
 
         /* set intensity */
         spi(0x0A, 0x0C);
@@ -57,24 +51,35 @@ void main(int argc, char** argv) {
 
         usleep(200);
 
-        /* send some test data */
-        spi(0x01, 0x37);
-        spi(0x02, 0x30);
-
-        sleep(5);
-
-        /* send diff test data */
-        spi(0x01, 0x30);
-        spi(0x02, 0x5B);
-
-/*        shutdown();*/
+        /* trying to set all segments on for digit 1.
+	for each digit (which is actually now the segment) set it high
+        this makes sense but shows nothing
+	for common cathode, the digit pin must be held high until the digit is active when it goes low
+	so under the normal setup, digit 0 to display all segments would set digit 0 pin to low, and then turn on all segments supplying current. 
+	now we've reversed that, and we connect our segment A to digit 0's anode (which would go high when active as desired)
+        Then each segment is connected to the digits and we must one by one set them to the target value
+	this is done by cycling through each digit and setting the relevant segment on or off
+	Instead of reading the binary values for each digit horizontally, we must know think of them vertically where we read the bit at position[digit] for each address (segment)
+	 */
+        spi(0x01, 0x60);
+        spi(0x02, 0x60);
+        spi(0x03, 0x60);
+        spi(0x04, 0x60);
+        spi(0x05, 0x60);
+        spi(0x06, 0x60);
+        spi(0x07, 0x60);
+        spi(0x08, 0x60);
 /*
-	for (;;) {
-                spi(0xFF, 0xFF);
-		sleep(1);
-                spi(0xFF, 0x00);
-		sleep(0);
-	}*/
+        spi(0x01, 0x20);
+        spi(0x02, 0x20);
+        spi(0x03, 0x20);
+        spi(0x04, 0x20);
+        spi(0x05, 0x20);
+        spi(0x06, 0x20);
+        spi(0x07, 0x20);
+        spi(0x08, 0x20);
+*/
+
 }
 
 
