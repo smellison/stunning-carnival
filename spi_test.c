ec0f2c0ccc23d475b52f808c9640ff50b094a450
ellison <gentlemenbandits@gmail.com>
2017-10-31 05:03:50 +0000

    Added static mapping functions for converting characters and numeric chars to hex and writing them out to all displays

--- spi_test.c
+++ spi_test.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <ctype.h>
 
 #include <wiringPiSPI.h>
 
@@ -30,7 +31,7 @@ void bootone() {
         spi(0x02, 0x07);
 
         /* set scan limit to one digit 0x00, six is 0x05 */
-        spi(0x03, 0x00);
+        spi(0x03, 0x05);
 
         /* control register PIRTEBxS
 	P - Blink Phase Readback 0 is p1 blink phase, 1 is p0 blink phase
@@ -52,19 +53,157 @@ void bootone() {
         /* Display Test, off */
 	spi(0x07, 0x00);
 
-        /* 
-	Everything else:
-	Key masks
-	Digit Type
-	Intesities
-	Digit 0 - 7a 0x60 -> 0x6F
-	Keys Debounced
-	Keys Pressed
-	*/
 }
 
 void shutdown() {
     spi(0x04, 0x00);
+}
+
+uint8_t get_digit_addr(int current_index) {
+	uint8_t addr;
+	switch (current_index) {
+		case 0:
+			addr = 0x20;
+			break;
+		case 1:
+			addr = 0x21;
+			break;
+		case 2:
+			addr = 0x22;
+			break;
+		case 3:
+			addr = 0x23;
+			break;
+		case 4:
+			addr = 0x24;
+			break;
+		case 5:
+			addr = 0x25;
+			break;
+		default:
+			addr = 0x20;
+	}
+	return addr;
+}
+
+void print_seq(char chr){
+	/* something like a for loop over each character, tracking position to know which char to print */
+	int index;
+	for(index=0; index < 8; index = index + 1) {
+		uint8_t addr = get_digit_addr(index);
+		switch(tolower(chr)) {
+			case 'a':
+				spi(addr,0x41);
+				break;
+			case 'b':
+				spi(addr,0x42);
+				break;
+			case 'c':
+				spi(addr,0x43);
+				break;
+			case 'd':
+				spi(addr,0x44);
+				break;
+			case 'e':
+				spi(addr,0x45);
+				break;
+			case 'f':
+				spi(addr,0x46);
+				break;
+			case 'g':
+				spi(addr,0x47);
+				break;
+			case 'h':
+				spi(addr,0x48);
+				break;
+			case 'i':
+				spi(addr,0x49);
+				break;
+			case 'j':
+				spi(addr,0x4A);
+				break;
+			case 'k':
+				spi(addr,0x4B);
+				break;
+			case 'l':
+				spi(addr,0x4C);
+				break;
+			case 'm':
+				spi(addr,0x4D);
+				break;
+			case 'n':
+				spi(addr,0x4E);
+				break;
+			case 'o':
+				spi(addr,0x4F);
+				break;
+			case 'p':
+				spi(addr,0x50);
+				break;
+			case 'q':
+				spi(addr,0x51);
+				break;
+			case 'r':
+				spi(addr,0x52);
+				break;
+			case 's':
+				spi(addr,0x53);
+				break;
+			case 't':
+				spi(addr,0x54);
+				break;
+			case 'u':
+				spi(addr,0x55);
+				break;
+			case 'v':
+				spi(addr,0x56);
+				break;
+			case 'w':
+				spi(addr,0x57);
+				break;
+			case 'x':
+				spi(addr,0x58);
+				break;
+			case 'y':
+				spi(addr,0x59);
+				break;
+			case 'z':
+				spi(addr,0x5A);
+				break;
+			case '0':
+				spi(addr,0x30);
+				break;
+			case '1':
+				spi(addr,0x31);
+				break;
+			case '2':
+				spi(addr,0x32);
+				break;
+			case '3':
+				spi(addr,0x33);
+				break;
+			case '4':
+				spi(addr,0x34);
+				break;
+			case '5':
+				spi(addr,0x35);
+				break;
+			case '6':
+				spi(addr,0x36);
+				break;
+			case '7':
+				spi(addr,0x37);
+				break;
+			case '8':
+				spi(addr,0x38);
+				break;
+			case '9':
+				spi(addr,0x39);
+				break;
+			default:
+				spi(addr,0x10); /* blank it */
+		}
+	}
 }
 
 void main(int argc, char** argv) {
@@ -75,8 +214,6 @@ void main(int argc, char** argv) {
         */
 
 	bootone();
-
-        usleep(200);
 
         /* trying to set all segments on for digit 1.
 	for each digit (which is actually now the segment) set it high
@@ -90,16 +227,37 @@ void main(int argc, char** argv) {
 	 */
 
         /* turn on display test */
-        /* spi(0x07, 0x01); */
+        /*spi(0x07, 0x01); */
+	/*sleep(5);*/
 
 	/* turn on digit 0, plane p0 with 0100 0010 */
-        spi(0x20,0x42);
+        /*spi(0x20,0x42);*/
 
-	sleep(2);
-
-        /* B? */
-	spi(0x20,0x4D);
-
+	print_seq('a');
+	sleep(1);
+	print_seq('b');
+	sleep(1);
+	print_seq('C');
+	sleep(1);
+	print_seq('X');
+	sleep(1);
+	print_seq('r');
+	sleep(1);
+	print_seq('d');
+	sleep(1);
+	print_seq('e');
+	sleep(1);
+	print_seq('f');
+	sleep(1);
+	print_seq('g');
+	sleep(1);
+	print_seq('h');
+	sleep(1);
+	print_seq('i');
+	sleep(1);
+	print_seq('j');
+	sleep(1);
+	print_seq('k');
 }
 
 
